9e8ac379c9caee823d30b40ca88f8315906edda1
Tobias Reich <tobias.reich.ich@gmail.com>
2016-01-05 14:44:36 +0100

    Fixed JSDoc

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let compile = require('./lib/compile'),
  * Returns an object of functions.
  * Those functions are public and accessible when importing the module.
  * @public
- * @param {array} routes - Array of route configurations.
+ * @param {Array} routes - Array of route configurations.
  * @returns {object}
  */
 module.exports = function(routes) {
