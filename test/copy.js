6e72f342f3ef6ae4fea0a33c8bfa22c55b8d531c
Tobias Reich <tobias.reich.ich@gmail.com>
2017-01-27 14:59:02 +0100

    New file creation for each file test task

--- test/copy.js
+++ test/copy.js
@@ -6,58 +6,36 @@ const assert = require('chai').assert
 const temp   = require('temp').track()
 const copy   = require('./../src/copy')
 
-let srcPath = null
+const newFile = function(opts) {
 
-let fileEJS = null
-let fileJS  = null
-let fileSWP = null
-let fileCSS = null
+	const file = temp.openSync(opts)
+
+	fs.writeFileSync(file.path, opts.content || '')
+
+	return file.path
+
+}
 
 describe('copy()', function() {
 
-	before(function() {
-
-		srcPath = temp.mkdirSync()
-
-		fileEJS = temp.openSync({
-			dir    : srcPath,
-			suffix : '.ejs'
-		}).path
-
-		fileJS = temp.openSync({
-			dir    : srcPath,
-			suffix : '.js'
-		}).path
-
-		fileSWP = temp.openSync({
-			dir    : srcPath,
-			suffix : '.swp'
-		}).path
-
-		fileCSS = temp.openSync({
-			dir    : srcPath,
-			suffix : '.css'
-		}).path
-
-		fs.writeFileSync(fileEJS)
-		fs.writeFileSync(fileJS)
-		fs.writeFileSync(fileSWP)
-		fs.writeFileSync(fileCSS)
-
-	})
-
 	it('should copy a directory without ignored files', function(next) {
 
+		const srcPath  = temp.mkdirSync()
 		const distPath = temp.mkdirSync()
+
+		const fileEJS = newFile({ dir: srcPath, suffix: '.ejs' })
+		const fileJS  = newFile({ dir: srcPath, suffix: '.js' })
+		const fileSWP = newFile({ dir: srcPath, suffix: '.swp' })
+		const fileCSS = newFile({ dir: srcPath, suffix: '.css' })
 
 		const distFileEJS = path.resolve(distPath, path.parse(fileEJS).base)
 		const distFileJS  = path.resolve(distPath, path.parse(fileJS).base)
 		const distFileSWP = path.resolve(distPath, path.parse(fileSWP).base)
 		const distFileCSS = path.resolve(distPath, path.parse(fileCSS).base)
 
-		const routes = [{
-			path: '[^_]*.{html,ejs}*'
-		}]
+		const routes = [
+			{ path: '[^_]*.{html,ejs}*' }
+		]
 
 		const opts = {
 			verbose : true,
