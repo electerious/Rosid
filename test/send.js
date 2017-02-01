b32da30be5ab115b5b0fe7a251c435728df72389
Tobias Reich <tobias.reich.ich@gmail.com>
2017-02-01 14:40:52 +0100

    Send tests

---
+++ test/send.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const assert = require('chai').assert
+const send   = require('./../src/send')
+
+describe('send()', function() {
+
+	it('should set content-type', function() {
+
+		const contentType = 'text/css'
+
+		const res = {
+			end: () => {},
+			setHeader: (key, value) => {
+
+				assert.strictEqual(key, 'Content-Type')
+				assert.strictEqual(value, contentType)
+
+			}
+		}
+
+		send(res, contentType, '')
+
+	})
+
+	it('should send data', function() {
+
+		const data = 'data'
+
+		const res = {
+			setHeader: () => {},
+			end: (_data) => {
+
+				assert.strictEqual(_data, data)
+
+			}
+		}
+
+		send(res, null, data)
+
+	})
+
+})
