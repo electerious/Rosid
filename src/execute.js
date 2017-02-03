4c55ef64dba5d499f7d29045935f137b412b94dc
Tobias Reich <tobias.reich.ich@gmail.com>
2017-02-03 11:08:27 +0100

    Don't edit opts directly

--- src/execute.js
+++ src/execute.js
@@ -27,7 +27,7 @@ module.exports = function(route, fileRoute, filePath, optimize, next) {
 
 	}
 
-	const opts = Object.assign(route.opts, {
+	const opts = Object.assign({}, route.opts, {
 		optimize: (route.opts.optimize===undefined ? optimize : route.opts.optimize)
 	})
 
