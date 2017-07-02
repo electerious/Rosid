ee18f51f7df6fd40d11a222ac51b53ed4d9ebb7a
Tobias Reich <tobias.reich.ich@gmail.com>
2017-07-02 18:22:00 +0200

    Match against relative path in copy module

--- src/copy.js
+++ src/copy.js
@@ -1,20 +1,18 @@
 'use strict'
 
-const path       = require('path')
-const fse        = require('fs-extra')
-const junk       = require('junk')
-const mm         = require('micromatch')
-const globEscape = require('glob-escape')
-const log        = require('./log')
+const path = require('path')
+const fse  = require('fs-extra')
+const junk = require('junk')
+const mm   = require('micromatch')
+const log  = require('./log')
 
 /**
  * Get a list of files which should not be copied.
  * @param {Array} routes - Array of route configurations.
  * @param {Array} customFiles - Array of user-defined globs.
- * @param {String} srcPath - Path to the source folder.
  * @returns {Array} ignoredFiles
  */
-const getIgnoredFiles = function(routes, customFiles, srcPath) {
+const getIgnoredFiles = function(routes, customFiles) {
 
 	// Always ignore the following files
 	const ignoredFiles = [
@@ -26,11 +24,8 @@ const getIgnoredFiles = function(routes, customFiles, srcPath) {
 		'**/.wafpickle-N'
 	]
 
-	// Escape glob patterns. srcPath should not glob.
-	const saveSrcPath = globEscape(srcPath)
-
-	// Make route paths absolute and ignore them
-	const ignoredRoutes = routes.map((route) => path.join(saveSrcPath, route.path))
+	// Extract the path out of the routes
+	const ignoredRoutes = routes.map((route) => route.path)
 
 	// Return all ignored files
 	return [
@@ -52,32 +47,36 @@ const getIgnoredFiles = function(routes, customFiles, srcPath) {
  */
 module.exports = function(routes, srcPath, distPath, opts, next) {
 
-	const ignoredFiles = getIgnoredFiles(routes, opts.ignore, srcPath)
+	const ignoredFiles = getIgnoredFiles(routes, opts.ignore)
 
-	const fseOpts = {
-		filter: (filePath) => {
+	const filter = (filePath) => {
 
-			const fileName = path.parse(filePath).base
+		// The filePath is absolute, but should be relative as micromatch will
+		// match against the relative path of the routes. Matching against
+		// an absolute path requires the use of path.join which causes issues on Windows:
+		// https://github.com/micromatch/micromatch/issues/95
+		filePath = path.relative(srcPath, filePath)
 
-			const isIgnored = mm.any(filePath, ignoredFiles)
-			const isJunk    = junk.is(fileName)
+		const fileName = path.parse(filePath).base
 
-			// Copy file when it's not ignored or not junk
-			const copy = isIgnored===false && isJunk===false
+		const isIgnored = mm.any(filePath, ignoredFiles)
+		const isJunk    = junk.is(fileName)
 
-			if (opts.verbose===true) {
+		// Copy file when it's not ignored or not junk
+		const copy = isIgnored===false && isJunk===false
 
-				if (copy===false) log(`{cyan:Skipping file: {grey:${ filePath }`)
-				if (copy===true)  log(`{cyan:Copying file: {grey:${ filePath }`)
+		if (opts.verbose===true) {
 
-			}
-
-			// Return true to include, false to exclude
-			return copy
+			if (copy===false) log(`{cyan:Skipping file: {grey:${ filePath }`)
+			if (copy===true)  log(`{cyan:Copying file: {grey:${ filePath }`)
 
 		}
+
+		// Return true to include, false to exclude
+		return copy
+
 	}
 
-	fse.copy(srcPath, distPath, fseOpts, next)
+	fse.copy(srcPath, distPath, { filter }, next)
 
 }
