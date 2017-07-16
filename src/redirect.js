55413070eb332663dacf0fb7b036e84163cf9938
Tobias Reich <tobias.reich.ich@gmail.com>
2017-07-16 15:49:48 +0200

    Syntax adjustment

--- src/redirect.js
+++ src/redirect.js
@@ -10,17 +10,14 @@ module.exports = function() {
 
 	return (req, res, next) => {
 
-		// Parse the URL
-		const _url = url.parse(req.url)
-
-		// Get the last char of the requested URL pathname
-		const lastChar = _url.pathname.substr(-1)
+		const parsedURL = url.parse(req.url)
+		const lastChar  = parsedURL.pathname.substr(-1)
 
 		// Only continue when requested URL is a folder
 		if (lastChar!=='/') return next()
 
 		res.statusCode = 302
-		res.setHeader('Location', `${ _url.pathname }index.html`)
+		res.setHeader('Location', `${ parsedURL.pathname }index.html`)
 		res.setHeader('Content-Length', '0')
 		res.end()
 
